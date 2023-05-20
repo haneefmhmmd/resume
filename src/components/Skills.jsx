8482feaf1021815ca061d11154064171fdf9df7a
haneefmhmmd <haneefmhmmd@gmail.com>
2023-05-19 23:35:54 -0400

    added ui improvements

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,34 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { v4 as uuid } from "uuid";
+import { ReactComponent as CSharp } from "../assets/Csharp.svg";
+import { ReactComponent as CSS } from "../assets/Css.svg";
+import { ReactComponent as Git } from "../assets/Git.svg";
+import { ReactComponent as HTML } from "../assets/Html.svg";
+import { ReactComponent as Java } from "../assets/Java.svg";
+import { ReactComponent as JS } from "../assets/Js.svg";
+import { ReactComponent as Next } from "../assets/NextJS.svg";
+import { ReactComponent as React } from "../assets/React.svg";
 import { ContentContext } from "../context/Provider";
 import Section from "./Section";
+
 export default function Skills() {
   const { skills: data } = useContext(ContentContext);
+  const iconMapper = {
+    HTML: <HTML />,
+    CSS: <CSS />,
+    JavaScript: <JS />,
+    "React.js": <React />,
+    "Next.js": <Next />,
+    Git: <Git />,
+    Java: <Java />,
+    "C#": <CSharp />,
+  };
   return (
     <Section title={data.title} id="skills">
       <ul className="skills-container">
         {data.list.map((skill) => (
           <li className="tag" key={uuid()}>
+            {iconMapper[skill]}
             <span className="tag__text">{skill}</span>
           </li>
         ))}
