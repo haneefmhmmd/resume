0c1647313c2b9d76e0406d93b4983057c6a8d8ef
haneefmhmmd <haneefmhmmd@gmail.com>
2024-01-23 20:26:34 -0500

    updated skill set

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { v4 as uuid } from "uuid";
+import { ReactComponent as Angular } from "../assets/Angular.svg";
 import { ReactComponent as CSharp } from "../assets/Csharp.svg";
 import { ReactComponent as CSS } from "../assets/Css.svg";
 import { ReactComponent as Git } from "../assets/Git.svg";
@@ -8,6 +9,7 @@ import { ReactComponent as Java } from "../assets/Java.svg";
 import { ReactComponent as JS } from "../assets/Js.svg";
 import { ReactComponent as Next } from "../assets/NextJS.svg";
 import { ReactComponent as React } from "../assets/React.svg";
+import { ReactComponent as SpringBoot } from "../assets/SpringBoot.svg";
 import { ContentContext } from "../context/Provider";
 import Section from "./Section";
 
@@ -17,11 +19,13 @@ export default function Skills() {
     HTML: <HTML />,
     CSS: <CSS />,
     JavaScript: <JS />,
+    Angular: <Angular />,
     "React.js": <React />,
     "Next.js": <Next />,
     Git: <Git />,
     Java: <Java />,
     "C#": <CSharp />,
+    "Spring Boot": <SpringBoot />,
   };
   return (
     <Section title={data.title} id="skills">
