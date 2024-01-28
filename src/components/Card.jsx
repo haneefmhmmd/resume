38b27bbad8ff57291b883294f2ba385eeea84337
haneefmhmmd <haneefmhmmd@gmail.com>
2024-01-28 13:02:44 -0500

    added action buttons to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,22 @@ import React from "react";
 
 export default function Card({ src, link = "", title = "", desc = "" }) {
   return (
-    <a className="card" href={link} target="_blank" rel="noreferrer">
+    <div className="card">
       <picture className="card__img">
         <img src={src} alt={title} />
       </picture>
       <h3 className="card__title">{title}</h3>
       <p className="card__desc" dangerouslySetInnerHTML={{ __html: desc }} />
-    </a>
+      <div className="card__action">
+        <a
+          className="btn btn--primary hero__cta"
+          href={link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          View Project
+        </a>
+      </div>
+    </div>
   );
 }
