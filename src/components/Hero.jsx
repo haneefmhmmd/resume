9bf29a1720ba5a3b5b6ed2af7da9aed05fee0665
haneefmhmmd <haneefmhmmd@gmail.com>
2024-02-12 20:03:46 -0500

    added typing text animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,19 @@
+import { motion } from "framer-motion";
 import React, { useContext } from "react";
 import { ContentContext } from "../context/Provider";
+import CursorBlinker from "./CursorBlinker";
+import TypingText from "./TypingText";
 
 export default function Hero() {
   const { hero: data } = useContext(ContentContext);
   return (
     <section className="container hero" id="#">
       <h2>{data.headline}</h2>
-      <h1>{data.name}</h1>
+      <motion.h1>
+        <TypingText text={data.name} />
+        <CursorBlinker />
+      </motion.h1>
+
       <p>{data.desc}</p>
       <a className="btn btn--primary hero__cta" href={data.cta.href}>
         <span className="btn__label">{data.cta.text}</span>
---
+++ src/components/TypingText.jsx
@@ -0,0 +1,42 @@
+import { animate, motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
+
+export default function TypingText({ text }) {
+  const textIndex = useMotionValue(0);
+  const texts = [text];
+  console.log(texts);
+
+  const baseText = useTransform(textIndex, (latest) => texts[latest] || "");
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const displayText = useTransform(rounded, (latest) =>
+    baseText.get().slice(0, latest)
+  );
+  const updatedThisRound = useMotionValue(true);
+
+  useEffect(() => {
+    animate(count, 30, {
+      type: "tween",
+      duration: 2.5,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+      repeatDelay: 1,
+      onUpdate(latest) {
+        if (updatedThisRound.get() === true && latest > 0) {
+          updatedThisRound.set(false);
+        } else if (updatedThisRound.get() === false && latest === 0) {
+          if (textIndex.get() === texts.length - 1) {
+            textIndex.set(0);
+          } else {
+            textIndex.set(textIndex.get() + 1);
+          }
+          updatedThisRound.set(true);
+        }
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <motion.span>{displayText}</motion.span>;
+}
